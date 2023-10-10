43014c5bfc9a3f26e28bdb856dea57132f44d722
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-10 16:11:31 -0400

    ko resource, spawn ko

--- scripts/counters/clear_counter.gd
+++ scripts/counters/clear_counter.gd
@@ -1,5 +1,11 @@
 class_name ClearCounter
 extends StaticBody3D
 
+@export var resource: KitchenObjectResource
+
+@onready var top_point := $CounterTopPoint
+
 func interact():
-	print("interacting with ", self)
+	var ko := resource.scene.instantiate()
+	ko.position = top_point.position
+	add_child(ko)
