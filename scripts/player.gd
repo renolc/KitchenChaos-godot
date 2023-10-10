6847aabbe97ef9184792fc05b7e2b212e1f11e44
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-10 11:37:56 -0400

    play anims when walking

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
+class_name Player
 extends CharacterBody3D
 
 @export var move_speed := 7.0
 @export var rot_speed := 0.3
 
-func _process(_delta):
+var is_walking := false
+
+func _process(delta):
 	var input_vector := Input.get_vector("left", "right", "up", "down")
 	velocity = Vector3(input_vector.x, 0, input_vector.y) * move_speed
-	if !velocity.is_equal_approx(Vector3.ZERO):
+	is_walking = !velocity.is_equal_approx(Vector3.ZERO)
+	if is_walking:
 		rotation.y = lerp_angle(rotation.y, atan2(velocity.x, velocity.z), rot_speed)
 	move_and_slide()
---
+++ scripts/player_visuals.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var player: Player
+
+@onready var animation_tree = $AnimationTree
+
+var Conditions = [
+	"is_idle",
+	"is_walking"
+]
+
+func _ready():
+	animation_tree.active = true
+
+func set_condition(c):
+	for i in Conditions:
+		animation_tree["parameters/conditions/" + i] = i == c
+
+func _process(delta):
+	var condition = "is_walking" if player.is_walking else "is_idle"
+	set_condition(condition)
