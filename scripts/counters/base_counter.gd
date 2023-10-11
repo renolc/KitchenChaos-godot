8094089ee2284a074a8b9969cf1cc12197aa77bb
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-11 13:01:58 -0400

    move ko stuff into base counter

--- scripts/counters/base_counter.gd
+++ scripts/counters/base_counter.gd
@@ -2,3 +2,19 @@ class_name BaseCounter
 extends StaticBody3D
 
 @onready var top_point := $CounterTopPoint
+
+var ko: KitchenObject
+
+func interact():
+	print("not implemented on ", self)
+
+func remove_kitchen_object():
+	top_point.remove_child(ko)
+	ko = null
+
+func set_kitchen_object(v: KitchenObject):
+	ko = v
+	top_point.add_child(ko)
+
+func has_kitchen_object() -> bool:
+	return !!ko
--- scripts/counters/clear_counter.gd
+++ scripts/counters/clear_counter.gd
@@ -1,21 +1,8 @@
 extends BaseCounter
 
-var ko: KitchenObject
-
 func interact():
 	if !ko:
 		if Player.Instance.has_kitchen_object():
 			Player.Instance.ko.holder = self
 	else:
 		ko.holder = Player.Instance
-
-func remove_kitchen_object():
-	self.top_point.remove_child(ko)
-	ko = null
-
-func set_kitchen_object(v: KitchenObject):
-	ko = v
-	self.top_point.add_child(ko)
-
-func has_kitchen_object() -> bool:
-	return !!ko
