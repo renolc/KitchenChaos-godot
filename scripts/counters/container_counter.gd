3b294243b841a761f59c3a802228257e8db4c4fc
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-11 12:00:51 -0400

    container sprite pulls from resource

--- scripts/counters/container_counter.gd
+++ scripts/counters/container_counter.gd
@@ -2,6 +2,11 @@ extends BaseCounter
 
 @export var resource: KitchenObjectResource
 
+@onready var sprite := $"ContainerCounterVisuals/ContainerCounter_Visual/ContainerCounter_Visual2/Single door/ObjectSprite/Sprite3D"
+
+func _ready():
+	sprite.texture = resource.sprite
+
 func interact():
 	var ko = resource.scene.instantiate() as KitchenObject
 	ko.holder = Player.Instance
