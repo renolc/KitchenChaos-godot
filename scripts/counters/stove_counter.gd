0adb5dd7ee4943211e23f925ce8bf9c9f0b396a6
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-12 14:31:12 -0400

    cook progress bar

--- scripts/counters/stove_counter.gd
+++ scripts/counters/stove_counter.gd
@@ -1,15 +1,22 @@
 extends BaseCounter
 
+signal cook_progress_update(progress)
+
 var fry_timer: SceneTreeTimer
 
+func _process(_delta):
+	if fry_timer:
+		cook_progress_update.emit(1 - (fry_timer.time_left / ko.cook_time))
+
 func interact():
-	if !ko:
+	if !has_kitchen_object():
 		if Player.Instance.has_kitchen_object() && Player.Instance.ko.cooks_into:
 			Player.Instance.ko.holder = self
 			start_cooking()
-	else:
+	elif !Player.Instance.has_kitchen_object():
 		ko.holder = Player.Instance
-		if fry_timer.time_left > 0:
+		cook_progress_update.emit(0)
+		if fry_timer && fry_timer.time_left > 0:
 			fry_timer.timeout.disconnect(finished_cooking)
 			fry_timer.timeout.emit()
 			fry_timer = null
@@ -20,6 +27,8 @@ func start_cooking():
 		fry_timer.timeout.connect(finished_cooking)
 
 func finished_cooking():
+	fry_timer = null
+	cook_progress_update.emit(0)
 	var new_ko := ko.cooks_into.instantiate() as KitchenObject
 	remove_kitchen_object(true)
 	new_ko.holder = self
