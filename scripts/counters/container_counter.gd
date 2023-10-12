683717846c30921b69088c9b701e2640754f6704
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-12 05:21:38 -0400

    move visual code into visual node

--- scripts/counters/container_counter.gd
+++ scripts/counters/container_counter.gd
@@ -3,16 +3,7 @@ extends BaseCounter
 
 @export var scene: PackedScene
 
-@onready var sprite := $"ContainerCounterVisuals/ContainerCounter_Visual/ContainerCounter_Visual2/Single door/ObjectSprite/Sprite3D"
-
 signal player_grabbed_object
-
-func _ready():
-	if !scene: return
-
-	var tmp := scene.instantiate() as KitchenObject
-	sprite.texture = tmp.sprite
-	tmp.queue_free()
 
 func interact():
 	if Player.Instance.has_kitchen_object(): return
--- scripts/counters/container_counter_visual.gd
+++ scripts/counters/container_counter_visual.gd
@@ -2,11 +2,16 @@ extends CounterVisual
 
 @onready var door := $"ContainerCounter_Visual/ContainerCounter_Visual2/Single door"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var sprite: Sprite3D = $"ContainerCounter_Visual/ContainerCounter_Visual2/Single door/ObjectSprite/Sprite3D"
 
 func _ready() -> void:
 	super()
-	(counter as ContainerCounter).player_grabbed_object.connect(play_open_anim)
+	var parent := counter as ContainerCounter
+	parent.player_grabbed_object.connect(play_open_anim)
+
+	var tmp := parent.scene.instantiate() as KitchenObject
+	sprite.texture = tmp.sprite
+	tmp.queue_free()
 
 func play_open_anim():
 	animation_player.play("open")
