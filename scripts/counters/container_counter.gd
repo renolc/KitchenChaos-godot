4b7a70786e0c8a019818d66bed4f07d0e8aac28b
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-12 05:36:13 -0400

    we can see container contents in editor

--- scripts/counters/container_counter.gd
+++ scripts/counters/container_counter.gd
@@ -1,7 +1,14 @@
+@tool
 class_name ContainerCounter
 extends BaseCounter
 
-@export var scene: PackedScene
+@export var scene: PackedScene:
+	set(v):
+		scene = v
+		if !Engine.is_editor_hint() || !visual: return
+		visual.set_sprite(scene)
+
+@onready var visual := $ContainerCounterVisuals
 
 signal player_grabbed_object
 
--- scripts/counters/container_counter_visual.gd
+++ scripts/counters/container_counter_visual.gd
@@ -1,3 +1,4 @@
+@tool
 extends CounterVisual
 
 @onready var door := $"ContainerCounter_Visual/ContainerCounter_Visual2/Single door"
@@ -5,11 +6,22 @@ extends CounterVisual
 @onready var sprite: Sprite3D = $"ContainerCounter_Visual/ContainerCounter_Visual2/Single door/ObjectSprite/Sprite3D"
 
 func _ready() -> void:
-	super()
-	var parent := counter as ContainerCounter
-	parent.player_grabbed_object.connect(play_open_anim)
+	if !Engine.is_editor_hint(): super()
 
-	var tmp := parent.scene.instantiate() as KitchenObject
+	var parent := counter as ContainerCounter
+	if !parent: return
+
+	if !Engine.is_editor_hint():
+		parent.player_grabbed_object.connect(play_open_anim)
+
+	set_sprite(parent.scene)
+
+func set_sprite(scene: PackedScene):
+	if !scene:
+		sprite.texture = null
+		return
+
+	var tmp := scene.instantiate() as KitchenObject
 	sprite.texture = tmp.sprite
 	tmp.queue_free()
 
