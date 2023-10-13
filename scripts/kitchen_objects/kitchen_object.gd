e2f4e1186ab7b3ace0b745ba1c4338609716f38d
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-13 06:01:59 -0400

    can only plate specific ingredients

--- scripts/kitchen_objects/kitchen_object.gd
+++ scripts/kitchen_objects/kitchen_object.gd
@@ -6,20 +6,25 @@ extends Node3D
 @export var cuts_required: int
 @export var cooks_into: PackedScene
 @export var cook_time: float
+@export var can_be_plated: bool
 
 var holder:
-	set(v):
-		if !v.has_method("set_kitchen_object"): return
-		if v.has_kitchen_object() && !v.ko.is_plate(): return
+	set(new_holder):
+		if !new_holder.has_method("set_kitchen_object"): return
+
+		var plated = false
+		if new_holder.has_kitchen_object():
+			if !new_holder.ko.is_plate(): return
+
+			if !(new_holder.ko as Plate).try_add_ingredient(self): return
+			else: plated = true
 
 		if holder:
 			holder.remove_kitchen_object()
 
-		if v.has_kitchen_object() && v.ko.is_plate():
-			(v.ko as Plate).add_ingredient(self)
-			return
+		if plated: return
 
-		holder = v
+		holder = new_holder
 		holder.set_kitchen_object(self)
 		position = Vector3.ZERO
 
--- scripts/kitchen_objects/plate_kitchen_object.gd
+++ scripts/kitchen_objects/plate_kitchen_object.gd
@@ -3,5 +3,9 @@ extends KitchenObject
 
 var added_ingredients: Array[KitchenObject] = []
 
-func add_ingredient(ko: KitchenObject):
+func try_add_ingredient(ko: KitchenObject) -> bool:
+	if !ko.can_be_plated: return false
+	if added_ingredients.any(func(i): return i.name == ko.name): return false
+
 	added_ingredients.push_back(ko)
+	return true
