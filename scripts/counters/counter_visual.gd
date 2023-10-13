05cb72b9ac8408ac74a894225f70f75ca6bd4288
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-13 10:19:25 -0400

    remove selected nodes -- use material overlay

--- scripts/counters/counter_visual.gd
+++ scripts/counters/counter_visual.gd
@@ -1,10 +1,13 @@
 class_name CounterVisual
 extends Node
 
-@onready var selected := $Selected
+@export var mesh_array: Array[MeshInstance3D]
+
+var selected_material: Material = preload("res://materials/counter_selected.tres")
 
 func _ready():
 	var counter = get_parent()
-	Player.Instance.selected_counter_changed.connect(func(i):
-		selected.visible = i == counter
+	Player.Instance.selected_counter_changed.connect(func(selected):
+		for i in mesh_array:
+			i.material_overlay = selected_material if selected == counter else null
 	)
