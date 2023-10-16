26f50f4e05ecbcd606568654b31350ff5c2c80ae
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-16 11:01:02 -0400

    stove particles

--- scripts/counters/stove_counter_visual.gd
+++ scripts/counters/stove_counter_visual.gd
@@ -1,6 +1,8 @@
 extends CounterVisual
 
 @onready var on_visual: MeshInstance3D = $StoveCounter_Visual/StoveCounter_Visual2/StoveOnVisual
+@onready var particles: GPUParticles3D = $StoveCounter_Visual/GPUParticles3D
 
 func progress_update(v: float):
 	on_visual.visible = v > 0 && v < 1
+	particles.emitting = on_visual.visible
