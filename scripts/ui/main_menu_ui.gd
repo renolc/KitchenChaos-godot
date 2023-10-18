485a0f9c44b028edf6d41fbe147381ae229b3c15
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-18 12:31:41 -0400

    loading screen, bg loading

--- scripts/ui/main_menu_ui.gd
+++ scripts/ui/main_menu_ui.gd
@@ -1,7 +1,16 @@
 extends Control
 
+@export_file("*.tscn") var game_scene_path
+
+@onready var loading_screen: ColorRect = $LoadingScreen
+
+func _ready():
+	ResourceLoader.load_threaded_request(game_scene_path)
+
 func play_pressed():
-	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+	loading_screen.visible = true
+	await get_tree().create_timer(0.1).timeout
+	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(game_scene_path))
 
 func quit_pressed():
 	get_tree().quit()
