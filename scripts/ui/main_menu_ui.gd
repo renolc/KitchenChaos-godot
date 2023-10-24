3e1edbe50573f08722f12e35359a0a87fb40937c
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-24 09:20:32 -0400

    don't bg load game (to prevent quit during load)

--- scripts/ui/main_menu_ui.gd
+++ scripts/ui/main_menu_ui.gd
@@ -7,13 +7,12 @@ extends Control
 
 func _ready():
 	get_tree().paused = false
-	ResourceLoader.load_threaded_request(game_scene_path)
 	play_btn.grab_focus()
 
 func play_pressed():
 	loading_screen.show()
 	await get_tree().create_timer(0.1).timeout
-	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(game_scene_path))
+	get_tree().change_scene_to_file(game_scene_path)
 
 func quit_pressed():
 	get_tree().quit()
