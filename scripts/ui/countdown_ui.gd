503136cb3ab4d5b6272100e59affcee73edbcb96
Phillip Gibson <phillipgibson@fastmail.com>
2023-10-25 09:20:59 -0400

    countdown timer anim and sfx

--- scripts/ui/countdown_ui.gd
+++ scripts/ui/countdown_ui.gd
@@ -1,10 +1,20 @@
 extends Control
 
 @onready var label: Label = $Label
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var countdown_sfx_player: SfxManager = $CountdownSfxManager
 
-func _process(_delta):
-	if visible:
-		label.text = str(ceil(GameManager.Instance.timer.time_left))
+func show_number():
+	label.text = str(ceil(GameManager.Instance.timer.time_left))
+	animation_player.play("count")
+	countdown_sfx_player.play_random()
+	await animation_player.animation_finished
+	if visible: show_number()
 
 func state_changed(state: GameManager.State):
-	visible = state == GameManager.State.Countdown
+	if state == GameManager.State.Countdown:
+		show()
+		await get_tree().create_timer(0.1).timeout
+		show_number()
+	else:
+		hide()
